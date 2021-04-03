72d7e42a9462f517b2f1531af47d57eff35ff168
walid777-hub <81866654+walid777-hub@users.noreply.github.com>
2021-04-03 15:40:26 +0100

    Add files via upload

---
+++ Adam Suleiman kagara.py
@@ -0,0 +1,25 @@
+print("NAME:Adam Suleiman Kagara")
+print("DEPARTMENT: Computer Science")
+scare = "Boo!"
+print (scare)
+var1 = 60
+var2 = 22
+third_score= var1 + var2
+print(var1, var2, third_score)
+var5 = 12
+var4 = .5
+var6 = var5 / var4
+print (var6)
+one = 90
+two = 56
+three = one + two
+print (three)
+dash = 8
+crash = dash % 3
+print (crash)
+A1 = 2
+A2 = A1 * 4
+B1 = 4
+B2 = B1 + 2
+print (A2 * B1 + 2)
+
